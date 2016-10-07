f1e946632f97be769f2b635dee35312250cdc142
akaliza <florakalisa@yahoo.fr>
2016-10-07 07:21:35 +0200

    quick union

--- src/main/java/algorithms/dynamicConnectivity/QuickUnionUF.java
+++ src/main/java/algorithms/dynamicConnectivity/QuickUnionUF.java
@@ -3,5 +3,33 @@ package algorithms.dynamicConnectivity;
 /**
  * Created by florakalisa on 10/4/16.
  */
-public class QuickUnionUF {
+public class QuickUnionUF implements UnionFind{
+
+    private int [] id;
+
+    public QuickUnionUF(int N){
+
+        id = new int[N];
+        for(int i =0; i < id.length; i ++){
+            id[i] = i; //N arrays accesses
+        }
+    }
+
+
+    private int root(int i){
+
+        while(i != id[i])  i = id[i];
+        return i;
+
+    }
+    public void union(int p, int q){
+        int i = root(p);
+        int j = root(p);
+        id[i] = j;
+    }
+
+    public boolean connected(int p, int q){
+        return root(p) ==root(q);
+    }
+
 }
