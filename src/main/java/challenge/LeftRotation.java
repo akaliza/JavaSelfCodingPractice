1a77f447c5cccd136ddc49844a07af33028253a4
akaliza <florakalisa@yahoo.fr>
2016-10-16 22:43:53 +0200

    iterator pattern and queue

--- src/main/java/challenge/LeftRotation.java
+++ src/main/java/challenge/LeftRotation.java
@@ -1,7 +1,27 @@
 package challenge;
 
+import java.util.Arrays;
+
 /**
  * Created by florakalisa on 10/15/16.
  */
 public class LeftRotation {
+
+    public static void main(String[] args) {
+
+
+        int[] myArray = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(myArray));
+        int rotationNumber = 4;
+        while (rotationNumber != 0) {
+            for (int i = 0; i < myArray.length - 1; i++) {
+                int tmp = myArray[i];
+                myArray[i] = myArray[i + 1];
+                myArray[i + 1] = tmp;
+            }
+            rotationNumber--;
+        }
+        System.out.println(Arrays.toString(myArray));
+
+    }
 }
