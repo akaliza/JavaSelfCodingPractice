226945a4622dc8123b1418f715645b3c4773cfd7
akaliza <florakalisa@yahoo.fr>
2016-10-23 18:23:55 +0200

    more sorting algorithm

--- src/main/java/algorithms/SelectionSort.java
+++ src/main/java/algorithms/SelectionSort.java
@@ -6,9 +6,12 @@ import java.util.Arrays;
 
 /**
  * Created by florakalisa on 10/19/16.
- * The idea : in iteration i find indes min of the smallest remianing, swap a[i] and a[min]
- * Time complexity : Quadratique = N*N
+ * The idea : in iteration i, find index min of the smallest remaining, swap a[i] and a[min]
+ * Time complexity : Quadratique = N*N/2 compares, N exchanges
  * Space complexity :
+ * Invariant : entries to the left of i fixed and in ascending order
+ *             No entry to the right of i is smaller than any entry to the left of i
+ *
  */
 public class SelectionSort {
 
@@ -23,17 +26,19 @@ public class SelectionSort {
     public static void sort(Comparable[] table) {
 
         for (int i = 0; i < table.length; i++) {
+            int min = i;
             for (int j = i + 1; j < table.length; j++) {
-                if (less(table[i], table[j]) == 1) {
-                    swap(table, i, j);
+                if (less(table[j], table[min]) == -1) {
+                    min = j;
                 }
             }
+            swap(table, i, min);
         }
     }
 
     private static void swap(Comparable[] table, int i, int j) {
         Comparable tmp = table[i];
-            table[i] = table[j];
+        table[i] = table[j];
         table[j] = tmp;
     }
 
