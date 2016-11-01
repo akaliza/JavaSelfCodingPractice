3dacf40967dbadb1cdd8cd2156aeec8f570a7ddb
akaliza <florakalisa@yahoo.fr>
2016-11-01 22:19:42 +0100

    design pattern - immutability

--- src/main/java/designpatterns/TestImmutability.java
+++ src/main/java/designpatterns/TestImmutability.java
@@ -2,12 +2,50 @@ package designpatterns;
 
 /**
  * Created by florakalisa on 10/19/16.
- * Time
+ * Immutable cannot change after object creation
+ * The only to change the object is to create a new one.
+ * Immutables are stable and fault tolerant and can be shared freely
+ * Immutables may lead to performance issue
+ * => all variables finales,
+ * => no setter methods,
+ * => for any change, we need to create a new object
+ * => protect the class to be extended
+ * => Ensure exclusive access to any mutable attributes
+ *
+ *
  */
 public class TestImmutability {
 
     public static void main(String[] args) {
 
+        ImmutableAccount immutableAccount = new ImmutableAccount(8000);
+        immutableAccount =  immutableAccount.withdraw(2000);
+        System.out.println(immutableAccount.getBalance());
+    }
+}
 
+final class ImmutableAccount {
+
+
+    private final long balance;
+
+    public ImmutableAccount (long balance){
+        validate(balance);
+        this.balance = balance;
+    }
+
+    private void validate(long balance) {
+        if(balance < 0)
+            throw new IllegalArgumentException("Balance cannot be negative");
+    }
+
+
+    public ImmutableAccount withdraw(long amount){
+        long newBalance = this.balance - amount;
+        return new ImmutableAccount(newBalance);
+    }
+
+    public long getBalance() {
+        return balance;
     }
 }
